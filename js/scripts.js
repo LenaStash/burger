17c125cc0b7d41595d764760272374d9fddc9eb9
Elena Stashevskaya <lenastashevskaya@gmail.com>
2020-04-08 16:12:52 +0300

    исправлены недочеты

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
-const deliveryForm = document.querySelector('#form');
+const deliveryForm = document.querySelector('#order-form');
 const deliverySubmit = document.querySelector('#send');
 deliverySubmit.addEventListener('click', e => {
     e.preventDefault();
-    if (!validateForm(deliveryForm)) {
+    if (validateForm(deliveryForm)) {
         var formData = new FormData(deliveryForm);
         formData.append("name", "deliveryForm.elements.name.value");
         formData.append("phone", "delivery.elements.phone.value");
         formData.append("comment", "deliveryForm.elements.comment.value");
-        formData.append("to", "test@test.ru");
+        formData.append("to", "lenokstash@gmail.com");
 
         var xhr = new XMLHttpRequest();
         xhr.responseType = 'json';
@@ -34,135 +34,19 @@ deliverySubmit.addEventListener('click', e => {
 function validateForm(form) {
     let valid = true;
 
-    if (!validateField(deliveryForm.elements.name)) {
+    if (!validateField(form.elements.name)) {
         valid = false;
     }
-    if (!validateField(deliveryForm.elements.phone)) {
+    if (!validateField(form.elements.phone)) {
         valid = false;
     }
-    if (!validateField(deliveryForm.elements.comment)) {
+    if (!validateField(form.elements.comment)) {
         valid = false;
     }
+    return valid;
 }
 
 function validateField(field) {
     field.nextElementSibling.textContent = field.validationMessage;
     return field.checkValidity();
-}
-
-///////ops
-
-const sections = $(".section");
-const display = $(".maincontent");
-
-let inScroll = false;
-
-const md = new MobileDetect(window.navigator.userAgent);
-const isMobile = md.mobile();
-
-const countSectionPosition = (sectionEq) => {
-
-    const position = sectionEq * -100;
-    if (isNaN(position))
-        console.error("передано не верное значение в countSectionPositon");
-
-    return position;
-};
-
-const resetActiveClass = (item, eq) => {
-    item.eq(eq).addClass("active").siblings().removeClass("active");
-};
-
-const performTransition = (sectionEq) => {
-    if (inScroll) return;
-
-    inScroll = true;
-
-    const position = countSectionPosition(sectionEq);
-    const trasitionOver = 1000;
-    const mouseInertionOver = 300;
-
-    resetActiveClass(sections, sectionEq);
-
-    display.css({
-        transform: `translateY(${position}%)`,
-    });
-
-    setTimeout(() => {
-        resetActiveClass($(".fixed-menu__item"), sectionEq);
-        inScroll = false;
-    }, trasitionOver + mouseInertionOver);
-};
-
-const scroller = () => {
-    const activeSection = sections.filter(".active");
-    const nextSection = activeSection.next();
-    const prevSection = activeSection.prev();
-
-    return {
-        next() {
-            if (nextSection.length) {
-                performTransition(nextSection.index());
-            }
-        },
-        prev() {
-            if (prevSection.length) {
-                performTransition(prevSection.index());
-            }
-        },
-    };
-};
-
-$(window).on("wheel", (e) => {
-    const deltaY = e.originalEvent.deltaY;
-    const windowScroller = scroller();
-
-    if (deltaY > 0) {
-        windowScroller.next();
-    }
-
-    if (deltaY < 0) {
-        windowScroller.prev();
-    }
-});
-
-$(document).on("keydown", (e) => {
-    const tagName = e.target.tagName.toLowerCase();
-    const windowScroller = scroller();
-    const userTypingInInputs = tagName === "input" || tagName === "textarea";
-
-    if (userTypingInInputs) return;
-
-    switch (e.keyCode) {
-        case 38:
-            windowScroller.prev();
-            break;
-        case 40:
-            windowScroller.next();
-            break;
-    }
-});
-
-$("[data-scroll-to]").on("click", (e) => {
-    e.preventDefault();
-
-    const $this = $(e.currentTarget);
-    const target = $this.attr("data-scroll-to");
-
-    performTransition(target);
-});
-
-if (isMobile) {
-    // https://github.com/mattbryson/TouchSwipe-Jquery-Plugin
-    $("body").swipe({
-        swipe: (event, direction) => {
-            let scrollDirection;
-            const windowScroller = scroller();
-
-            if (direction === "up") scrollDirection = "next";
-            if (direction === "down") scrollDirection = "prev";
-
-            windowScroller[scrollDirection]();
-        },
-    });
 }
